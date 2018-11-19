e210952c4231f1b473f2b5ff4886402ac7bf3f41
KingKiller3000 <bassat101@hotmail.com>
2018-11-19 12:44:21 +0100

    Done met gewone opdracht

---
+++ WebOpdracht2.1/Controllers/StudentAdministratieController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebOpdracht2._1.Models;
+
+namespace WebOpdracht2._1.Controllers
+{
+    public class StudentAdministratieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ZoekStudenten(char voorletter)
+        {
+            List<Student> Studenten = new List<Student>();
+            Student piet = new Student()
+            {
+                voornaam = "piet",
+                achternaam = "Alexander"
+
+            };
+            Student jan = new Student()
+            {
+                voornaam = "jan",
+                achternaam = "Hendriksen"
+
+            };
+            Student klaas = new Student()
+            {
+                voornaam = "klaas",
+                achternaam = "Willem"
+
+            };
+            Studenten.Add(piet);
+            Studenten.Add(jan);
+            Studenten.Add(klaas);
+
+            foreach (Student s in Studenten.ToList())
+            {
+                if (!s.achternaam.StartsWith(voorletter))
+                {
+                    Studenten.Remove(s);
+                }
+            }
+
+            return View(Studenten);
+        }
+
+    }
+}
---
+++ WebOpdracht2.1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebOpdracht2._1.Models
+{
+    public class Student
+    {
+        public string voornaam { set; get; }
+        public string achternaam { set; get; }
+    }
+}
